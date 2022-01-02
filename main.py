701bb45e90579a643890050cdd9d2be786bb4a72
Adawari <adawarithomas@gmail.com>
2022-01-02 11:05:23 +0000

    create food class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -9,6 +10,7 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.go_up, "Up")
@@ -19,15 +21,29 @@ screen.onkey(snake.turn_right, "Right")
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(snake.speed)
     screen.update()
     snake.move()
 
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280:
+    if snake.head.xcor() > 280:
         snake.reappear_x()
 
-    if snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if snake.head.xcor() < -280:
+        snake.reappear_neg_x()
+
+    if snake.head.ycor() > 280:
         snake.reappear_y()
+
+    if snake.head.ycor() < -280:
+        snake.reappear_neg_y()
+    
+    if snake.head.distance(food) < 20:
+        snake.extend()
+        food.refresh()
+        snake.speed_up()
+
+
+
 
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import time
 
 STARTING_POSITION = [(0,0), (-20, 0), (-40, 0)]
 UP = 90
@@ -11,6 +12,7 @@ class Snake:
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
+        self.speed = 0.25
 
     def create_snake(self):
         for position in STARTING_POSITION:
@@ -27,6 +29,15 @@ class Snake:
             self.segments[seg_num].goto(new_x, new_y)
         
         self.head.forward(20)
+
+    def extend(self):
+        x_pos = self.segments[-1].xcor()
+        y_pos = self.segments[-1].ycor()
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        self.segments.append(new_segment)
+        new_segment.goto(x_pos, y_pos)
 
     def go_up(self):
         if self.head.heading() != DOWN:
@@ -45,13 +56,24 @@ class Snake:
             self.head.setheading(RIGHT)
 
     def reappear_x(self):
-        x_position = self.head.xcor()
         y_position = self.head.ycor()
-        new_x = x_position * -1
-        self.head.goto(new_x, y_position)
+        self.head.goto(-280, y_position)
+    
+    def reappear_neg_x(self):
+        y_position = self.head.ycor()
+        self.head.goto(280, y_position)
 
     def reappear_y(self):
         x_position = self.head.xcor()
-        y_position = self.head.ycor()
-        new_y = y_position * -1
-        self.head.goto(x_position, new_y)
+        self.head.goto(x_position, -280)
+
+    def reappear_neg_y(self):
+        x_position = self.head.xcor()
+        self.head.goto(x_position, 280)
+
+    def refresh(self):
+        self.head.goto(0, 0)
+
+    def speed_up(self):
+        self.speed *= 0.9
+        time.sleep(self.speed)
